6e1eb2561a61bbb0d21fc6db5797b948ee79885d
Risskov <arisskov@gmail.com>
2021-11-04 15:39:24 +0100

    controllers

--- robot_controller/src/move_robot.py
+++ robot_controller/src/move_robot.py
@@ -13,14 +13,14 @@ class ServoControl:
         self.control = controller
         self.receive = receiver
         self.stop = False
-        self.velocity = 0.2
-        self.acceleration = 0.5
+        self.velocity = 0.1
+        self.acceleration = 0.3
         self.lookAheadTime = 0.1
         self.gain = 600
 
     def runTrajectory(self, goal, server):
         self.stop = False
-        current_pose = self.receive.getActualTCPPose()
+        current_pose = self.receive.getActualTCPPose()[0:3]+[0, 0, 0]
         trajectory = np.linspace(current_pose, goal.pose, 1000)
         pose = trajectory[0]  # Move the robot to the first pose in the trajectory
         self.control.moveL(pose)
@@ -58,24 +58,10 @@ class MoveRobot:
     def moveCallback(self, goal):
         self.servCon.runTrajectory(goal, self.move_server)
 
-
     def stopCallback(self, goal):
         self.servCon.stopTrajectory()
         self.stop_server.set_succeeded(StopRobotResult(True))
 
-    '''    
-    def callback(self, goal):
-        print(goal)
-        if goal.stop:
-            self.rtde_c.stopL(10)
-        else:
-            done = self.rtde_c.moveL(goal.pose, 0.25, 0.5, True)
-            while not done: pass
-
-        self.server.set_succeeded(MoveRobotResult(True))
-        #if goal failed
-        #self.server.set_succeeded(MoveRobotResult(False))
-    '''
 if __name__ == "__main__":
     rospy.init_node('move_robot')
     mr = MoveRobot()
