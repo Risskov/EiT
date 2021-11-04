4c2165c3e31cf255bcddb37673818523ca301c1a
Risskov <arisskov@gmail.com>
2021-11-04 14:59:47 +0100

    controllers

--- robot_controller/src/move_robot_client.py
+++ robot_controller/src/move_robot_client.py
@@ -4,6 +4,7 @@ import actionlib
 from robot_controller.msg import MoveRobotAction, MoveRobotGoal, StopRobotAction, StopRobotGoal
 import time
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 class Controller:
     def __init__(self):
@@ -30,20 +31,21 @@ class Controller:
     def feedbackCallback(self, feedback):
         force = np.linalg.norm(feedback.force)
         print("Force: ", force)
-        #if feedback > 5:
-        #    self.pose_reached = True
+        if force > 7:
+            self.pose_reached = True
 
     def waitForResult(self):
         while not self.pose_reached:
             time.sleep(0.01)
         self.pose_reached = False
 
-    def testTrajectory(self):
+    def testTrajectory(self, trajectory):
+        '''
         pose = [0.5154353428673626, -0.26885648388269845, -0.049974561539680856,
                 -0.0011797094876681562, 3.1162457375630024, 0.038842380117300054]
         self.sendPose(pose)
 
-        pose = [0.3154353428673626, -0.26885648388269845, -0.049974561539680856,
+        pose = [0.2154353428673626, -0.26885648388269845, -0.049974561539680856,
                 -0.0011797094876681562, 3.1162457375630024, 0.038842380117300054]
         self.sendPose(pose)
 
@@ -53,11 +55,32 @@ class Controller:
         time.sleep(1)
         self.client_stop.send_goal(StopRobotGoal())
         self.client_stop.wait_for_result()
-
-        print(self.client_stop.get_result())
+        '''
+        for traj in trajectory:
+            self.sendPose(traj)
+        #print(self.client_stop.get_result())
         print("done")
+
+def toBaseFrame(point):
+    pointnp = np.asarray(point)
+    transM = [0.13054, -0.24411, -0.03447, 0., 0., -1.17658]
+    r = R.from_euler("xyz", transM[3:]).as_matrix()
+    t = np.asarray([[1, 0, 0, transM[0]], [0, 1, 0, transM[1]], [0, 0, 1, transM[2]], [0, 0, 0, 1]])
+    t[0:3, 0:3] = r
+    pose = t @ pointnp
+    return (pose[0:3]/pose[3]).tolist()+[0, 3.1415, 0]
 
 if __name__ == "__main__":
     controller = Controller()
-    controller.testTrajectory()
+    pose0 = [0.4, -0.6, 0.1, 1]
+    pose1 = [0.4, 0.6, 0.1, 1]
+    point0BF = toBaseFrame(pose0)
+    point1BF = toBaseFrame(pose1)
+    print(point1BF)
+    #pose0 = [0.5154353428673626, -0.26885648388269845, -0.049974561539680856,
+    #         -0.0011797094876681562, 3.1162457375630024, 0.038842380117300054]
+    #pose1 = [0.1154353428673626, -0.26885648388269845, -0.049974561539680856,
+    #         -0.0011797094876681562, 3.1162457375630024, 0.038842380117300054]
+    traject = [point1BF, point0BF, point1BF, point0BF, point1BF]
+    controller.testTrajectory(traject)
 
