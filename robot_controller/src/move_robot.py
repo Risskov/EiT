d4ccd82a2a6170f1f6519edf99a9bea3f7f28982
Risskov <arisskov@gmail.com>
2021-11-17 10:10:58 +0100

    controllers

--- robot_controller/src/move_robot.py
+++ robot_controller/src/move_robot.py
@@ -44,8 +44,8 @@ class ServoControl:
 
     def move(self, goal, server):
         self.control.moveL(goal.pose, self.velocity, self.acceleration, True)
-        while receiver.getAsyncOperationProgress() >= 0 and not self.stop:
-            print("Progress: ", receiver.getAsyncOperationProgress())
+        while self.receiver.getAsyncOperationProgress() >= 0 and not self.stop:
+            print("Progress: ", self.receiver.getAsyncOperationProgress())
             server.publish_feedback(MoveRobotFeedback(force=self.receive.getActualTCPForce()))
         if self.stop:
             self.control.stopL()
